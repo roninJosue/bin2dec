441453232cdefa8ed7980b0d49ef3126936e4ac2
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-10-26 22:49:58 -0600

    feat: Add default style to Button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,24 @@
+import styled from "styled-components";
+
 const Button = () => {
+
+  const DefaultButton = styled.button`
+    min-width: 200px;
+    padding: .625rem;
+    border-radius: 10px;
+    border: solid #ccc;
+    border-width: 1px 1px 5px 1px;
+    text-align: center;
+    
+    &:active {
+      border-width: 1px;
+    }
+  `
+
   return(
-    <button>
+    <DefaultButton>
       Hello
-    </button>
+    </DefaultButton>
   )
 }
 
