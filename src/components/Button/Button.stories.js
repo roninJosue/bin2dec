08ad27eb9611ee4ee09ec05185738ec3673e1a27
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-10-29 22:27:10 -0600

    chore: Add button's primary story

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -5,15 +5,19 @@ export default {
   title: 'Converter/Button',
   component: Button,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 };
 
 const Template = (args) => <Button {...args} />;
 
 export const Default = Template.bind({})
+export const Primary = Template.bind({})
 
 Default.args = {
   label: 'Convert',
+  variant: 'default'
 };
+
+Primary.args = {
+  label: 'Convert',
+  variant: 'primary'
+}
