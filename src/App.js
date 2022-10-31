5f760df7d3f5663286f1304d2ba8d6394224c4d0
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-10-30 21:38:44 -0600

    feat: Import Heading and Button component to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button, Heading} from "./components";
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +20,8 @@ function App() {
         >
           Learn React
         </a>
+        <Button />
+        <Heading />
       </header>
     </div>
   );
