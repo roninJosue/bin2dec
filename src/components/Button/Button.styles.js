8cdd51be21bbd066a87c766fdf137932f7ba6dc3
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-10-30 20:00:49 -0600

    refactor: Separate styles from component

---
+++ src/components/Button/Button.styles.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+const colors = {
+  primary: {
+    color: '#02044A',
+    bgColor: '#74C0FC',
+    borderColor: '#02044A'
+  },
+  default: {
+    color: '#000',
+    bgColor: '',
+    borderColor: '#ccc'
+  }
+}
+
+const getColor = (variant) => {
+  return colors[variant] || colors['default']
+}
+
+export const StyledButton = styled.button`
+  min-width: 200px;
+  padding: .625rem;
+  border-radius: 10px;
+  color: ${({variant}) => getColor(variant).color};
+  background-color: ${({variant}) => getColor(variant).bgColor};
+  border: solid ${({variant}) => getColor(variant).borderColor};
+  border-width: 1px 1px 5px 1px;
+  text-align: center;
+  cursor: pointer;
+  position: relative;
+
+  &:active {
+    border-bottom: 1px solid ${({variant}) => getColor(variant).borderColor};
+    top: 3px;
+  }
+`
