f509ccf9d1edf509cf77abb426ba06558a2b3145
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-10-31 17:30:12 -0600

    feat: Export Result component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Button from './Button/Button';
 import Heading from './Heading/Heading';
 import Paragraph from './Paragraph/Paragraph';
+import Result from './Result/Result';
 
-export { Button, Heading, Paragraph };
+export { Button, Heading, Paragraph, Result };
