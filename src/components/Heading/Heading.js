db818a9aa8fb7e89a01cb4db6294cc73b5210392
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-10-31 10:44:11 -0600

    refactor: Remove return statement

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -1,7 +1,5 @@
 import StyledHeading from "./Heading.styles";
 
-const Heading = ({text='Heading', level = 2}) => {
-  return(<StyledHeading level={level}>{text}</StyledHeading>)
-}
+const Heading = ({text='Heading', level = 1}) => (<StyledHeading level={level}>{text}</StyledHeading>)
 
 export default Heading
