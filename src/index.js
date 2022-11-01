bad128dc3d502e792b7f7ad6c06d9a53b3ebcfd5
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-11-01 09:29:31 -0600

    feat: Add GlobalStyle

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import { GlobalStyle } from './styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
