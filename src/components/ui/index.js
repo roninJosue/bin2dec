63800c0c7c5fc22f1447be00cfd8937238e63fa9
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-11-02 22:35:41 -0600

    refactor: Export Label and Input component

---
+++ src/components/ui/index.js
@@ -0,0 +1,9 @@
+import Button from './Button/Button';
+import Heading from './Heading/Heading';
+import Paragraph from './Paragraph/Paragraph';
+import Result from './Result/Result';
+import Container from '../Container/Container';
+import Label from './Label/Label';
+import Input from './Input/Input';
+
+export { Button, Heading, Paragraph, Result, Container, Label, Input };
