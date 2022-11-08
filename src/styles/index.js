297eb6309d9c18e753f157c0a2ec44571c87098b
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-11-07 23:48:45 -0600

    feat: Add css styles to the root element

--- src/styles/index.js
+++ src/styles/index.js
@@ -11,4 +11,12 @@ export const GlobalStyle = createGlobalStyle`
     font-size: 100%;
     font-family: 'Outfit', sans-serif;
   }
+  
+  #root{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #9EC4FB;
+  }
 `;
