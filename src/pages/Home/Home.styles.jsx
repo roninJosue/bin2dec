d0c16651501acb218935d5e2ac547e7961704f0d
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-11-07 23:51:53 -0600

    feat: Set width to the home section

--- src/pages/Home/Home.styles.jsx
+++ src/pages/Home/Home.styles.jsx
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
-export const StyledHome = styled.div`
-  width: 60%;
-  //height: 80vh;
-  padding: 5em;
+export const StyledHome = styled.section`
+  width: 70%;
+  //height: 70vh;
+  padding: 8em 5em;
   background-color: #02044a;
   display: flex;
   justify-content: space-between;
