fcd837a33308cb1662a3f57d8fccd7db94da9910
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-11-11 22:25:33 -0600

    feat: Set form's width to 100% in small device

--- src/components/FormConverter/FormConverter.styles.jsx
+++ src/components/FormConverter/FormConverter.styles.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { device } from '../../styles/utils';
 
 export const StyledForm = styled.form`
   padding: 2em;
@@ -6,9 +7,14 @@ export const StyledForm = styled.form`
   border-radius: 15px;
   display: flex;
   flex-direction: column;
-  width: 350px;
+  width: 100%;
   height: 350px;
   & * {
     margin-bottom: 1em;
+  }
+
+  @media screen and ${device.tablet} {
+    padding: 1.5em;
+    width: 350px;
   }
 `;
