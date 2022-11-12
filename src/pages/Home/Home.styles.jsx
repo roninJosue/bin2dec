2a863fa2291eb4d39d70435f4c4ec69a7014101c
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-11-11 22:21:22 -0600

    feat: Add style for responsive design

--- src/pages/Home/Home.styles.jsx
+++ src/pages/Home/Home.styles.jsx
@@ -1,12 +1,44 @@
 import styled from 'styled-components';
+import { device } from '../../styles/utils';
 
 export const StyledHome = styled.section`
-  width: 70%;
-  //height: 70vh;
-  padding: 8em 5em;
   background-color: #02044a;
   display: flex;
   justify-content: space-between;
   align-items: start;
   border-radius: 15px;
+
+  @media screen and ${device.mobileS} {
+    flex-direction: column;
+    padding: 0;
+    border-radius: 0;
+  }
+  @media screen and ${device.mobileM} {
+    flex-direction: column;
+    padding: 1em;
+    border-radius: 0;
+  }
+
+  @media screen and ${device.mobileL} {
+    //flex-direction: row;
+    width: 100%;
+    padding: 2em;
+    align-items: center;
+  }
+
+  @media screen and ${device.tablet} {
+    width: 90%;
+    padding: 5em;
+    flex-direction: row;
+    border-radius: 15px;
+  }
+
+  @media screen and ${device.laptop} {
+    width: 70%;
+    padding: 8em 5em;
+  }
+
+  @media screen and ${device.desktop} {
+    width: 40%;
+  }
 `;
