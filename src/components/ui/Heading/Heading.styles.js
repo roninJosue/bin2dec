302d453723acff202b0dc890ad1af248ceb78bf9
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-11-11 22:23:26 -0600

    feat: Change font-size in small device

--- src/components/ui/Heading/Heading.styles.js
+++ src/components/ui/Heading/Heading.styles.js
@@ -1,15 +1,24 @@
 import styled from 'styled-components';
+import { device } from '../../../styles/utils';
 
 const HeadingBase = ({ level, as: Component = `h${level}`, ...props }) => (
   <Component {...props} />
 );
 
-const StyledHeading = styled(HeadingBase)({
-  margin: 0,
-  fontWeight: 'bold',
-  color: '#fff',
-  fontSize: '4.44em',
-  lineHeight: '1.1em',
-});
+const StyledHeading = styled(HeadingBase)`
+  margin: 0;
+  font-weight: bold;
+  color: #fff;
+  font-size: 4.44em;
+  line-height: 1.1em;
+
+  @media screen and ${device.mobileS} {
+    font-size: 2em;
+  }
+
+  @media screen and ${device.laptop} {
+    font-size: 3em;
+  }
+`;
 
 export default StyledHeading;
