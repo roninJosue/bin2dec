80953d8e4bee81e29f3370095c3d22cf53860f49
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-11-14 10:37:20 -0600

    feat: Add Home page to App component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,7 @@
-import logo from './logo.svg';
-import { Button, Heading, Paragraph } from './components';
+import Home from './pages/Home/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button />
-        <Heading />
-        <Paragraph>
-          Basic converter of binary numbers to decimal numbers, please introduce
-          only 0s and 1s and then hit the button
-        </Paragraph>
-      </header>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
