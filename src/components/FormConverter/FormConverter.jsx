287c156d76b5ca5b44917436e73d0e6e98f896af
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-11-16 16:47:58 -0600

    refactor: Add schema validation

--- src/components/FormConverter/FormConverter.jsx
+++ src/components/FormConverter/FormConverter.jsx
@@ -1,27 +1,30 @@
 import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
 import { Label, Button, Result, Input } from '../ui';
 import { StyledForm } from './FormConverter.styles';
+
+const validationSchema = Yup.object().shape({
+  binaryNumber: Yup.string()
+    .required('Binary number is required')
+    .matches(/^[01]+$/, 'Only 0s and 1s'),
+});
 
 const FormConverter = ({ result, handleChange }) => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm({ resolver: yupResolver(validationSchema) });
+
   const onSubmit = (data) => {
     handleChange(data);
   };
   return (
     <StyledForm onSubmit={handleSubmit(onSubmit)}>
       <Label text="Binary number" />
-      <Input
-        placeHolder="Binary Number"
-        {...register('binaryNumber', {
-          required: 'Binary Number is required',
-          pattern: /^[01]+$/,
-        })}
-      />
-      {errors.binaryNumber?.type === 'required' && (
+      <Input placeHolder="Binary Number" {...register('binaryNumber')} />
+      {errors.binaryNumber?.message && (
         <span>{errors.binaryNumber?.message}</span>
       )}
       <Button label="Convert" variant="primary" type="submit" />
