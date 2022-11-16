af98d92910da30734657381e301a3140d5d05fe1
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-11-15 21:55:15 -0600

    feat: Use the hook and pass properties to the form

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,16 @@
 import { Description, FormConverter } from '../../components';
 import { StyledHome } from './Home.styles';
+import useHome from '../../hooks/useHome';
 
-const Home = () => (
-  <StyledHome>
-    <Description />
-    <FormConverter />
-  </StyledHome>
-);
+const Home = () => {
+  const { result, handleChange } = useHome();
+
+  return (
+    <StyledHome>
+      <Description />
+      <FormConverter result={result} handleChange={handleChange} />
+    </StyledHome>
+  );
+};
 
 export default Home;
