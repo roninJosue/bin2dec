1bb319be4a88e9f4361010d7210543cca8bb751d
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-11-15 21:58:04 -0600

    feat: Add state to the form, and use React hook form

--- src/components/FormConverter/FormConverter.jsx
+++ src/components/FormConverter/FormConverter.jsx
@@ -1,18 +1,32 @@
-import { Label, Input, Button, Result } from '../ui';
+import { useForm } from 'react-hook-form';
+import { Label, Button, Result, Input } from '../ui';
 import { StyledForm } from './FormConverter.styles';
 
-const FormConverter = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Submit');
+const FormConverter = ({ result, handleChange }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    handleChange(data);
   };
   return (
-    <StyledForm onSubmit={handleSubmit}>
+    <StyledForm onSubmit={handleSubmit(onSubmit)}>
       <Label text="Binary number" />
-      <Input />
-      <Button label="Convert" variant="primary" />
+      <Input
+        placeHolder="Binary Number"
+        {...register('binaryNumber', {
+          required: 'Binary Number is required',
+          pattern: /^[01]+$/,
+        })}
+      />
+      {errors.binaryNumber?.type === 'required' && (
+        <span>{errors.binaryNumber?.message}</span>
+      )}
+      <Button label="Convert" variant="primary" type="submit" />
       <Label text="Result" />
-      <Result result={0} />
+      <Result result={result} />
     </StyledForm>
   );
 };
