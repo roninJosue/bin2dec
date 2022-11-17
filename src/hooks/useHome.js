908a830c8b00ead0da24aeb765fa104f7cb42310
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-11-16 22:13:09 -0600

    feat: Add logic for convert binary to decimal

--- src/hooks/useHome.js
+++ src/hooks/useHome.js
@@ -1,10 +1,16 @@
 import { useState } from 'react';
 
+const convertNumber = (binary) =>
+  binary
+    .map((number, index) => (number === '0' ? 0 : 2 ** index))
+    .filter((number) => number > 0)
+    .reduce((acc, cur) => acc + cur, 0);
+
 const useHome = () => {
   const [result, setResult] = useState(0);
 
   const handleChange = ({ binaryNumber = 0 }) => {
-    setResult(binaryNumber);
+    setResult(convertNumber(binaryNumber.split('').reverse()));
   };
 
   return {
