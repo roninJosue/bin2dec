0bbee8326ad53a7f6708d7713be40a91201e2775
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-11-16 22:10:46 -0600

    feat: Add Text component

---
+++ src/components/ui/Text/Text.jsx
@@ -0,0 +1,5 @@
+import { StyledText } from './Text.styles';
+
+const Text = ({ children }) => <StyledText>{children}</StyledText>;
+
+export default Text;
---
+++ src/components/ui/Text/Text.styles.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const StyledText = styled.span`
+  margin: 0.5em 0.5em 0;
+  text-align: center;
+`;
+
+export const ErrorText = styled(StyledText)`
+  color: #d32f2f;
+  font-weight: bold;
+`;
