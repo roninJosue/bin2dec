04af392ffe820571bab6dc5f6d9766826023c2d9
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-11-18 18:10:10 -0600

    feat: Set html font-size to 85% in mobile

--- src/styles/index.js
+++ src/styles/index.js
@@ -4,16 +4,20 @@ import { device } from './utils';
 
 export const GlobalStyle = createGlobalStyle`
   ${normalize}
-  * {
+  *, *:before, *:after {
     box-sizing: border-box;
   }
 
   html {
-    font-size: 100%;
+    font-size: 85%;
     font-family: 'Outfit', sans-serif;
+
+    @media screen and ${device.mobileL} {
+      font-size: 100%;
+    }
   }
-  
-  #root{
+
+  #root {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -24,7 +28,7 @@ export const GlobalStyle = createGlobalStyle`
     @media screen and ${device.mobileS} {
       padding: 1em;
     }
-    
+
     @media screen and ${device.tablet} {
       height: 100vh;
       background-color: #9EC4FB;
