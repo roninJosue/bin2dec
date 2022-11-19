77b3219cc2d260b12a630b93cb97cc0fafdba893
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-11-18 18:08:43 -0600

    feat: Vary the result's font size in base of the length

--- src/components/ui/Result/Result.styles.jsx
+++ src/components/ui/Result/Result.styles.jsx
@@ -1,8 +1,16 @@
 import styled from 'styled-components';
 
+const calculateFontSize = (length) => {
+  if (length <= 7) return '4em';
+  if (length <= 11) return '3em';
+  if (length <= 15) return '2em';
+
+  return '1.5em';
+};
 export const StyledResult = styled.div`
-  font-size: 4rem;
+  font-size: ${({ length }) => calculateFontSize(length)};
   font-weight: bold;
   color: #02044a;
   text-align: center;
+  //transition: font-size 1ms ease;
 `;
