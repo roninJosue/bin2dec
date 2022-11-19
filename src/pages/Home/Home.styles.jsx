bf9a8efef2f544f9dc2f8a7c3b5c8787ea807ea2
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-11-18 18:06:08 -0600

    feat: Set flex direction to column in mobile

--- src/pages/Home/Home.styles.jsx
+++ src/pages/Home/Home.styles.jsx
@@ -7,20 +7,18 @@ export const StyledHome = styled.section`
   justify-content: space-between;
   align-items: start;
   border-radius: 15px;
+  flex-direction: column;
 
   @media screen and ${device.mobileS} {
-    flex-direction: column;
     padding: 0;
     border-radius: 0;
   }
   @media screen and ${device.mobileM} {
-    flex-direction: column;
     padding: 1em;
     border-radius: 0;
   }
 
   @media screen and ${device.mobileL} {
-    //flex-direction: row;
     width: 100%;
     padding: 2em;
     align-items: center;
