78cf0910f16fe8df95baa785ed40176a8dcf85f9
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-11-22 22:26:42 -0600

    refactor: Add id to the input

--- src/components/ui/Input/Input.jsx
+++ src/components/ui/Input/Input.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import { StyledInput } from './Input.styles';
 
 const Input = React.forwardRef((props, ref) => (
-  <StyledInput ref={ref} {...props} placeholder={props.placeHolder} />
+  <StyledInput
+    type="text"
+    id={props.id}
+    ref={ref}
+    {...props}
+    placeholder={props.placeHolder}
+  />
 ));
 
 Input.displayName = 'Input';
