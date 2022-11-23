a9f97ddfca743a655fff4f1cb7ae5a4b5ebcd8d7
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-11-22 22:25:17 -0600

    test: Modify Description test, verify paragraph render

--- src/components/Description/Description.test.jsx
+++ src/components/Description/Description.test.jsx
@@ -4,6 +4,13 @@ import { Description } from '../index';
 describe('Description', () => {
   it('should render H1', () => {
     render(<Description />);
-    expect(screen.getByText(/Binary To Decimal/i)).toBeInTheDocument();
+    expect(screen.getByRole('heading')).toBeInTheDocument();
+  });
+
+  it('should render a paragraph', () => {
+    render(<Description />);
+    expect(
+      screen.getByText(/Basic converter of binary numbers/i)
+    ).toBeInTheDocument();
   });
 });
