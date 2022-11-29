b90fe0fa55b9b34d2830ce854b7a26f8ccd0c1fe
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-11-28 21:35:36 -0600

    test: Add test for render the Home page

--- src/pages/Home/Home.test.jsx
+++ src/pages/Home/Home.test.jsx
@@ -1,44 +1,25 @@
-import {
-  act,
-  fireEvent,
-  render,
-  screen,
-  waitFor,
-} from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Home from './Home';
-import useHome from '../../hooks/useHome';
 
-jest.mock('../../hooks/useHome');
+jest.mock('../../hooks/useHome', () => ({
+  __esModule: true,
+  default: () => ({
+    result: 0,
+    handleChange: jest.fn(),
+  }),
+}));
 
 describe('Home', () => {
-  beforeEach(() => {
-    useHome.mockReturnValue();
-  });
   it('render properly', async () => {
-    await act(() => {
-      render(<Home />);
-    });
+    render(<Home />);
 
-    await waitFor(async () => {
-      expect(await screen.getByRole('button')).toBeInTheDocument();
-      expect(await screen.getByTestId('result')).toHaveTextContent(0);
-    });
-  });
-
-  it('should display correct number', async () => {
-    await act(() => {
-      render(<Home />);
-    });
-
-    //await waitFor(async () => {
     fireEvent.input(screen.getByTestId('input-converter'), {
-      target: { value: '01' },
+      target: { value: 1010 },
     });
 
-    await act(() => {
-      fireEvent.submit(screen.getByRole('button'));
-    });
-    expect(await screen.getByTestId('result')).toHaveTextContent(2);
-    //});
+    expect(await screen.getByTestId('input-converter')).toHaveValue('1010');
+    expect(await screen.getByRole('button')).toBeInTheDocument();
+
+    expect(await screen.getByTestId('result')).toHaveTextContent(0);
   });
 });
